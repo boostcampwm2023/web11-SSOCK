bd83aaa739be2603ef536f30f9d6adb1bdd1fb04
kcwww <cwkim0321@gmail.com>
2023-11-08 03:40:26 +0900

    feat: add cylinder
    원기둥 추가 및 클릭 이벤트 테스트

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import Tree from './Tree';
-import Cube from './Cube';
+import Cylinder from './Cylinder';
 import Glass from './Glass';
 import { OrbitControls } from '@react-three/drei';
 import CustomCamera from './CustomCamera';
@@ -10,12 +10,18 @@ const App = () => {
   return (
     <Canvas>
       {/* <CustomCamera /> */}
+      { /* 
+      이 부분은
+      conflict 해결을 위해
+      테스트 할
+      부분입니다.
+      */}
       <OrbitControls />
       <ambientLight intensity={1} />
       <directionalLight position={[2, 1, 3]} intensity={1} />
       <Glass />
       <Tree />
-      <Cube />
+      <Cylinder />
     </Canvas>
   );
 };
