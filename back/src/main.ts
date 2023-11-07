5294cd35d23fbe020b467a8bed45e8210f88829e
송현우 <83938394+yunuo46@users.noreply.github.com>
2023-11-07 14:37:41 +0900

    feat: 프로젝트 초기 세팅

---
+++ back/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
