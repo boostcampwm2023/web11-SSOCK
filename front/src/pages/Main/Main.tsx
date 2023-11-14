3591c7082434ee7424f8e2076e13adc77127f943
esthel <esthel9026@naver.com>
2023-11-14 22:52:17 +0900

    feat: /main UI 1차 및 로그아웃 진행

--- front/src/pages/Main/Main.tsx
+++ front/src/pages/Main/Main.tsx
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+import theme from '../../utils/theme';
+import mock from '../../mockdata.json'; // temporary
+import { SnowGlobeCanvas } from '../../components';
+import MainButtonBox from './MainButtonBox';
+
+const StyledHeader = styled.div`
+  font: ${theme.font['--normal-main-header-font']};
+  text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
+  position: absolute;
+  top: 5%;
+  left: 50%;
+  white-space: nowrap;
+  transform: translate(-50%, 0);
+  color: white;
+`;
+
+const StyledUser = styled.span`
+  color: ${theme.colors['--nick-name']};
+  font-size: 20px;
+`;
+
 const Main = () => {
-  return <></>;
+  const userName = mock.user_name;
+
+  return (
+    <>
+      <SnowGlobeCanvas />
+
+      <StyledHeader>
+        <StyledUser>{userName}</StyledUser>님의 스노우볼
+      </StyledHeader>
+
+      <MainButtonBox />
+    </>
+  );
 };
 
 export default Main;
