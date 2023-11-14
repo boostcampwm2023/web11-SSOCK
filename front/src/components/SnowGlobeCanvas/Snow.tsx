36255cf541ad32b55a42d24e43c7e2733712286d
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-11-14 18:58:28 +0900

    remove: LiginPage폴더 삭제

--- front/src/components/SnowGlobeCanvas/Snow.tsx
+++ front/src/components/SnowGlobeCanvas/Snow.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { Vector3, useFrame } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
 
 import * as THREE from 'three';
 
--- front/src/pages/LoginPage/Introduce/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as Introduce } from './Introduce';
