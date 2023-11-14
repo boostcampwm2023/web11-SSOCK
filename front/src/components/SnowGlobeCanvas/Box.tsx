33d89683553adae6ab43bbca694c545746c8b98f
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-11-14 18:39:33 +0900

    feat,refact: 유리재질 변경
    최적화를 위해 유리재질 변경
    지평좌표계 고정(0,0,0)이 바닥 가운데
    SnowGlobe.tsx 삭제
    Canvas 구조 변경

--- front/src/components/SnowGlobeCanvas/Box.tsx
+++ front/src/components/SnowGlobeCanvas/Box.tsx
@@ -1,43 +1,22 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+import React from 'react';
 import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
-
-interface MyModelProps {
-  url: string;
-  scale: number;
-  position: THREE.Vector3;
-  color: THREE.Color;
-}
 interface BoxProps {
   scale: number;
   position: THREE.Vector3;
   color: THREE.Color;
 }
 
-const MyModel: React.FC<MyModelProps> = ({ url, scale, position, color }) => {
-  const { scene } = useGLTF(url);
+const Box: React.FC<BoxProps> = ({ scale, position, color }) => {
+  const { scene } = useGLTF('./testModel/1mBox.glb');
 
   const cube = scene.getObjectByName('Cube') as THREE.Mesh;
   cube.material = new THREE.MeshBasicMaterial({ color });
 
   cube.scale.set(scale, scale, scale);
   cube.position.set(position.x, position.y, position.z);
-  //console.log(gltf.scene);
-  return <primitive object={scene.clone()} />;
-};
 
-const Box: React.FC<BoxProps> = ({ scale, position, color }) => {
-  return (
-    <>
-      <MyModel
-        url={'./testModel/1mBox.glb'}
-        scale={scale}
-        position={position}
-        color={color}
-      />
-    </>
-  );
+  return <primitive object={scene.clone()} />;
 };
 
 export default Box;
