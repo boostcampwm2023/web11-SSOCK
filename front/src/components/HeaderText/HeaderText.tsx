daad60b86abed8e25537762cdce3efbdb36672c4
esthel <esthel9026@naver.com>
2023-11-17 02:00:31 +0900

    fix: header text 컴포넌트 적용

--- front/src/components/HeaderText/HeaderText.tsx
+++ front/src/components/HeaderText/HeaderText.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import theme from '../../utils/theme';
 import mock from '../../mockdata.json'; // temporary
 
+interface HeaderProps {
+  Ref: React.RefObject<HTMLDivElement> | null;
+}
+
 const StyledHeader = styled.div`
   font: ${theme.font['--normal-main-header-font']};
   text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
@@ -11,6 +15,17 @@ const StyledHeader = styled.div`
   white-space: nowrap;
   transform: translate(-50%, 0);
   color: white;
+
+  @keyframes fadeInUp1 {
+    from {
+      opacity: 1;
+      transform: translate(-50%, 0);
+    }
+    to {
+      opacity: 0;
+      transform: translate(-50%, -100%);
+    }
+  }
 `;
 
 const StyledUser = styled.span`
@@ -18,11 +33,11 @@ const StyledUser = styled.span`
   font-size: 20px;
 `;
 
-const HeaderText = (headerRef: React.RefObject<HTMLDivElement>) => {
+const HeaderText = (props: HeaderProps) => {
   const userName = mock.user_name;
 
   return (
-    <StyledHeader ref={headerRef ? headerRef : null}>
+    <StyledHeader ref={props ? props.Ref : null}>
       <StyledUser>{userName}</StyledUser>님의 스노우볼
     </StyledHeader>
   );
--- front/src/pages/Main/MainButtonBox.tsx
+++ front/src/pages/Main/MainButtonBox.tsx
@@ -3,34 +3,7 @@ import styled from 'styled-components';
 import theme from '../../utils/theme';
 import MenuModal from './MenuModal';
 import ListMsg from './ListMsg';
-import mock from '../../mockdata.json'; // temporary
-
-const StyledHeader = styled.div`
-  font: ${theme.font['--normal-main-header-font']};
-  text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
-  position: absolute;
-  top: 5%;
-  left: 50%;
-  white-space: nowrap;
-  transform: translate(-50%, 0);
-  color: white;
-
-  @keyframes fadeInUp1 {
-    from {
-      opacity: 1;
-      transform: translate(-50%, 0);
-    }
-    to {
-      opacity: 0;
-      transform: translate(-50%, -100%);
-    }
-  }
-`;
-
-const StyledUser = styled.span`
-  color: ${theme.colors['--nick-name']};
-  font-size: 20px;
-`;
+import { HeaderText } from '../../components';
 
 const StyledMenu = styled.img`
   position: absolute;
@@ -130,8 +103,6 @@ const screenTime = (
 };
 
 const MainButtonBox = () => {
-  const userName = mock.user_name;
-
   const headerRef = useRef<HTMLDivElement>(null);
   const menuRef = useRef<HTMLImageElement>(null);
   const screenRef = useRef<HTMLImageElement>(null);
@@ -146,9 +117,7 @@ const MainButtonBox = () => {
     <>
       {!screen ? (
         <>
-          <StyledHeader ref={headerRef}>
-            <StyledUser>{userName}</StyledUser>님의 스노우볼
-          </StyledHeader>
+          <HeaderText Ref={headerRef} />
 
           <StyledMenu
             ref={menuRef}
