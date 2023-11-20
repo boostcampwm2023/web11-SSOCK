62ecc07f700a413a0b07c594a426976252f95428
kcwww <cwkim0321@gmail.com>
2023-11-21 03:07:02 +0900

    styled component render

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,5 +1,5 @@
 export { Intro } from './Intro';
-export { Visit, Deco, VisitWrite } from './Visit';
+export { Visit, Deco } from './Visit';
 export { Nickname, Snowball } from './Make';
 export { Main } from './Main';
 export { Wrong } from './Wrong';
