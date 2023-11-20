bb5defac52eb2fa0314e89194797005a718da20a
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-11-20 12:47:34 +0900

    feat,add: deco상수선언, 장식모델 추가

---
+++ front/src/constants/deco.tsx
@@ -0,0 +1,14 @@
+const DECO = [
+  {
+    name: '선물상자',
+    fileName: 'giftbox.glb'
+  },
+  {
+    name: '진저브레드',
+    fileName: 'gingerBread.glb'
+  },
+  {
+    name: '붕어빵',
+    fileName: 'fishBread.glb'
+  }
+];
