372df930e04d8926fb390b755b0596a4daec0343
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-11-20 22:16:11 +0900

    refact: Object->Models로 변경

--- front/src/components/SnowGlobeCanvas/SnowGlobeCanvas.tsx
+++ front/src/components/SnowGlobeCanvas/SnowGlobeCanvas.tsx
@@ -6,7 +6,7 @@ import * as THREE from 'three';
 import { useRef } from 'react';
 import { getDecoPoisition } from '../../utils/position';
 import mock from '../../mockdata.json';
-import * as Object from './objects/index';
+import * as Models from './models/index';
 
 const CanvasBox = styled.div`
   margin: auto;
@@ -20,9 +20,8 @@ const SnowGlobeCanvas = () => {
   const isClicked = useRef<boolean>(false);
   const glassRadius = 7;
   const glassPosition = new THREE.Vector3(0, glassRadius / 2, 0);
-
   const snows = Array.from({ length: 100 }, (_, i) => (
-    <Object.Snow
+    <Models.Snow
       key={i}
       centerPosition={glassPosition}
       rangeRadius={glassRadius}
@@ -32,7 +31,7 @@ const SnowGlobeCanvas = () => {
   const decos = mock.snowball[0].message.map((deco, index) => {
     // console.log(deco);
     return (
-      <Object.Deco
+      <Models.Deco
         key={index}
         id={deco.deco_id}
         scale={1}
@@ -55,20 +54,20 @@ const SnowGlobeCanvas = () => {
           castShadow
         />
 
-        <Object.Raycaster isClickedRef={isClicked} />
-        <Object.Ground scale={1} position={new THREE.Vector3(0, 0, 0)} />
-        <Object.Glass
+        <Models.Raycaster isClickedRef={isClicked} />
+        <Models.Ground scale={1} position={new THREE.Vector3(0, 0, 0)} />
+        <Models.Glass
           position={new THREE.Vector3(0, glassRadius / 2, 0)}
           color={new THREE.Color('skyblue')}
           radius={glassRadius}
           opacity={0.1}
         />
-        <Object.MainDeco
+        <Models.MainDeco
           id={mock.snowball[0].main_deco_id}
           scale={1}
           position={new THREE.Vector3(0, 10, 0)}
         />
-        <Object.Bottom scale={1} position={new THREE.Vector3(0, 0, 0)} />
+        <Models.Bottom scale={1} position={new THREE.Vector3(0, 0, 0)} />
         {snows}
         {decos}
       </Canvas>
