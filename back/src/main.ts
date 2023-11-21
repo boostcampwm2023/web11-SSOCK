6e3e7d8b911d635a65e012bab636feb4bca2f083
송현우 <83938394+yunuo46@users.noreply.github.com>
2023-11-21 23:34:17 +0900

    feat: swagger 로그인 팝업 구현

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { setupSwagger } from 'src/util/swagger';
 import { AppModule } from './app.module';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    //이 부분 추가
+    ['/docs'], // docs(swagger end point)에 진입시
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD // 지정된 ID/비밀번호
+      }
+    })
+  );
   setupSwagger(app);
   await app.listen(3000);
 }
