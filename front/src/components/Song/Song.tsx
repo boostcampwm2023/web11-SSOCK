55d026108b10d15007d7bd8de37b1ade42ba3fff
esthel <esthel9026@naver.com>
2023-11-21 13:32:44 +0900

    fix: 음악 무한재생, 위치 조절

--- front/src/components/Song/Song.tsx
+++ front/src/components/Song/Song.tsx
@@ -4,16 +4,16 @@ import theme from '../../utils/theme';
 
 const StyledMusic = styled.img`
   position: absolute;
-  bottom: 0;
-  height: 6%;
-  right: 0;
+  top: 10%;
+  height: 4%;
+  margin-left: 2%;
   filter: invert(100%) sepia(82%) saturate(2%) hue-rotate(196deg)
     brightness(108%) contrast(101%);
   z-index: 100;
 
   @media (min-width: ${theme.size['--desktop-min-width']}) {
-    left: 50%;
-    margin-left: 450px;
+    right: 50%;
+    margin-right: 450px;
   }
 `;
 
@@ -21,6 +21,7 @@ const useAudio = (url: string) => {
   const [song] = useState(new Audio(url));
   const [play, setPlay] = useState(false);
 
+  song.loop = true;
   const toggle: MouseEventHandler<HTMLImageElement> = () => setPlay(!play);
 
   useEffect(() => {
