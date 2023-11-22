9edcba9d675eb55b761b78ffb8ad3ac34cda5ef2
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-11-22 16:06:20 +0900

    fix: build error fixed

--- front/src/pages/Visit/VisitBody.tsx
+++ front/src/pages/Visit/VisitBody.tsx
@@ -1,13 +1,8 @@
-import { useState, useEffect, useContext } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-import theme from '../../utils/theme';
-import { Button } from '../../components';
+import { useContext } from 'react';
 import { MessageContext } from './MessageProvider';
 import { Msg } from '../../components';
 
 const VisitBody = () => {
-  const navigate = useNavigate();
   const { message, sender, color } = useContext(MessageContext); // message가 '' 비어있지 않을때
 
   return (
