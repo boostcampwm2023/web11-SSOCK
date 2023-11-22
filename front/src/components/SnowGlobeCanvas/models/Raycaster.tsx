1ac1239c9e69e6456bb78c5513e992dafbcdb761
kcwww <cwkim0321@gmail.com>
2023-11-23 01:03:13 +0900

    fix: fix build error

--- front/src/components/SnowGlobeCanvas/models/Raycaster.tsx
+++ front/src/components/SnowGlobeCanvas/models/Raycaster.tsx
@@ -3,7 +3,6 @@ import { useThree, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { MessageContext } from '../../../pages/Visit/MessageProvider';
 import { PrevContext } from '../PrevProvider';
-import { is } from '@react-three/fiber/dist/declarations/src/core/utils';
 
 interface RaycasterProps {
   isClickedRef: React.MutableRefObject<boolean>; // mutable
