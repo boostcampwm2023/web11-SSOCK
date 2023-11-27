8c1d109e2091de712f4c87aa439a7657ac2d108b
peageon <jinsoo2005@gmail.com>
2023-11-27 17:54:53 +0900

    feat: enable Cors (민아요청)

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+  app.enableCors();
   setupSwagger(app);
   await app.listen(3000);
 }
