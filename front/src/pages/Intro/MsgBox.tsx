71d5db65fa67a692f7cbfdf2f6b44ea90b9708ab
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-12-03 15:25:18 +0900

    feat: intro msg띄우기 추가

---
+++ front/src/pages/Intro/MsgBox.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { Msg } from '../../components';
+import { MessageContext } from '../Visit/MessageProvider';
+import { SnowBallContext } from '../Visit/SnowBallProvider';
+
+const MsgBox = () => {
+  const { message, sender, color } = useContext(MessageContext); // message가 '' 비어있지 않을때
+  const { userData } = useContext(SnowBallContext);
+  return (
+    <>
+      {message !== '' ? (
+        <Msg
+          color={color}
+          isInput={false}
+          content={message}
+          sender={sender}
+          to={userData.nickname}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default MsgBox;
