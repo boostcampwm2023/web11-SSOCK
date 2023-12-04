78a637ae7d267af0406d7a49eb536cc8416e23ca
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-12-04 14:22:46 +0900

    feat: visit페이지 스노우볼 전환 구현
    전환은 되는데 메시지작성 post가 제대로 안됨

--- front/src/pages/Visit/VisitBody.tsx
+++ front/src/pages/Visit/VisitBody.tsx
@@ -1,17 +1,25 @@
 import { useContext } from 'react';
 import { Msg } from '../../components';
 import { MessageContext } from './MessageProvider';
-import { SnowBallContext } from './SnowBallProvider';
+import { SnowBallContext, SnowBallData, UserData } from './SnowBallProvider';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const MsgContainer = styled.div`
   max-height: fit-content;
   overflow: scroll;
 `;
 
+const Btn = styled.button`
+  color: white;
+`;
+
 const VisitBody = () => {
   const { message, sender, color } = useContext(MessageContext); // message가 '' 비어있지 않을때
-  const { userData } = useContext(SnowBallContext);
+  const { userData, snowBallData, setSnowBallData } =
+    useContext(SnowBallContext);
+  console.log('------------------------');
+  console.log(userData, snowBallData);
   return (
     <MsgContainer>
       {message !== '' ? (
@@ -22,6 +30,58 @@ const VisitBody = () => {
           sender={sender}
           to={userData.nickname}
         />
+      ) : null}
+      {userData.snowball_list.length > 0 ? (
+        <>
+          <Btn
+            onClick={e => {
+              const nowSnowBallID = userData.snowball_list.findIndex(
+                id => id == snowBallData.id
+              );
+              if (nowSnowBallID === undefined) {
+                throw '알수없는 snowballID입니다.';
+              }
+              const nextSnowBallID =
+                userData.snowball_list[
+                  (nowSnowBallID - 1) % userData.snowball_count
+                ];
+              axios(`/api/snowball/${nextSnowBallID}`)
+                .then(res => {
+                  console.log('test', res);
+                  setSnowBallData(res.data as SnowBallData);
+                })
+                .catch(e => {
+                  console.error(e);
+                });
+            }}
+          >
+            이전
+          </Btn>
+          <Btn
+            onClick={e => {
+              const nowSnowBallID = userData.snowball_list.findIndex(
+                id => id == snowBallData.id
+              );
+              if (nowSnowBallID === undefined) {
+                throw '알수없는 snowballID입니다.';
+              }
+              const nextSnowBallID =
+                userData.snowball_list[
+                  (nowSnowBallID + 1) % userData.snowball_count
+                ];
+              axios(`/api/snowball/${nextSnowBallID}`)
+                .then(res => {
+                  console.log('test', res);
+                  setSnowBallData(res.data as SnowBallData);
+                })
+                .catch(e => {
+                  console.error(e);
+                });
+            }}
+          >
+            다음
+          </Btn>
+        </>
       ) : null}
     </MsgContainer>
   );
