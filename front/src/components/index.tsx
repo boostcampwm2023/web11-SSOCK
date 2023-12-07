8d757cc02a079a3c110c7da52adb5f35c1055b1f
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2023-12-07 23:26:20 +0900

    feat: 메시지 읽음처리 API연동 완료~
    ㅎㅎ 너무 행복해요 ^^
    
    Co-authored-by: Mina Seo <esthel9026@naver.com>

--- front/src/components/index.tsx
+++ front/src/components/index.tsx
@@ -1,3 +1,4 @@
+export { UIContainer } from './UIContainer';
 export { Song } from './Song';
 export { SnowGlobeCanvas } from './SnowGlobeCanvas';
 export { Button, StepButton } from './Button';
@@ -5,5 +6,4 @@ export { InputSnowball } from './InputSnowball';
 export { HeaderText } from './HeaderText';
 export { Prev } from './Prev';
 export { Msg, ListMsg } from './Msg';
-export { UIContainer } from './UIContainer';
 export { DeleteModal } from './Modal';
