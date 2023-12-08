9eb6e6534886aa29277c01375cbe93a00dd5096e
esthel <esthel9026@naver.com>
2023-12-08 23:26:13 +0900

    feat: useLogout window open으로 연결

--- front/src/hooks/useLogout.tsx
+++ front/src/hooks/useLogout.tsx
@@ -1,17 +1,11 @@
-import axios from 'axios';
 import cookie from 'react-cookies';
 
 const useLogout = () => {
-  axios
-    .get('/api/auth/logout')
-    .then(res => (res.status === 200 ? window.location.replace('/') : null))
-    .catch(e => {
-      console.error(e);
-      cookie.remove('access_token');
-      cookie.remove('refresh_token');
-      cookie.remove('loggedin');
-      window.location.replace('/');
-    });
+  window.open(`/api/auth/logout`, '_self');
+  cookie.remove('access_token');
+  cookie.remove('refresh_token');
+  cookie.remove('loggedin');
+  window.location.replace('/');
 };
 
 export default useLogout;
