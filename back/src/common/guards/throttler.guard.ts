2fba4f8023d8345f4d12d66fdaaf905538bac0d1
송현우 <83938394+yunuo46@users.noreply.github.com>
2023-12-10 08:59:47 +0900

    fix: proxy 설정 테스트

--- back/src/common/guards/throttler.guard.ts
+++ back/src/common/guards/throttler.guard.ts
@@ -1,18 +1,13 @@
 import { ThrottlerGuard } from '@nestjs/throttler';
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const ip = await this.getTracker(request);
-    console.log(ip);
-    return true;
-  }
-
   protected async getTracker(req: Record<string, any>): Promise<string> {
     console.log('req.ip = ', req.ip);
     console.log('req.ips = ', req.ips);
+    const forwardedFor = req.headers['x-forwarded-for'];
+    console.log('forwardedFor = ', forwardedFor);
     return req.ips.length ? req.ips[0] : req.ip;
   }
 }
