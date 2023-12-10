963af9a7dfe58038f482847d1fb654790e900c80
송현우 <83938394+yunuo46@users.noreply.github.com>
2023-12-10 10:35:40 +0900

    fix: config skipIf 로직 삭제

--- back/src/config/throttler-config.ts
+++ back/src/config/throttler-config.ts
@@ -5,12 +5,7 @@ const throttlerConfig: ThrottlerModuleOptions = {
     {
       name: 'api',
       ttl: 10000, // 10 seconds
-      limit: 30,
-      skipIf: context => {
-        // api route로 시작하지 않는 요청은 제외
-        const req = context.switchToHttp().getRequest();
-        return req.route.path.startsWith('/api') ? false : true;
-      }
+      limit: 30
     }
   ],
   errorMessage: 'Too Many API Requests'
