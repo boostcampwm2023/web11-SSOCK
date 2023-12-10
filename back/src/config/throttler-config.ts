27fbc737cf866c3a8c19a810f631775c1852e986
송현우 <83938394+yunuo46@users.noreply.github.com>
2023-12-10 10:14:53 +0900

    feat: api route로 접근하지 않는 요청은 throttler 제외

--- back/src/config/throttler-config.ts
+++ back/src/config/throttler-config.ts
@@ -3,11 +3,17 @@ import { ThrottlerModuleOptions } from '@nestjs/throttler';
 const throttlerConfig: ThrottlerModuleOptions = {
   throttlers: [
     {
+      name: 'api',
       ttl: 10000, // 10 seconds
-      limit: 30
+      limit: 30,
+      skipIf: context => {
+        // api route로 시작하지 않는 요청은 제외
+        const req = context.switchToHttp().getRequest();
+        return req.route.path.startsWith('/api') ? false : true;
+      }
     }
   ],
-  errorMessage: 'Too Many Requests'
+  errorMessage: 'Too Many API Requests'
 };
 
 export default throttlerConfig;
