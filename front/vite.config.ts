dddeb859219534928b23d075e64ee5784c0abd9d
kcwww <cwkim0321@gmail.com>
2024-04-21 02:21:12 +0900

    refactor: add compress option

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+import { compression } from 'vite-plugin-compression2';
+
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(),  compression({
+    include : /\.(js|svg|ico|ttf|mp3)$/i,
+    threshold: 1400,
+  })],
   server: {
     host: '0.0.0.0',
     https: true,
